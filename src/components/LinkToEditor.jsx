024ced0f3c20f20398c98a5852de1d91bb6eb1e9
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2025-01-06 10:51:13 -0800

    Update LinkToEditor.jsx

--- src/components/LinkToEditor.jsx
+++ src/components/LinkToEditor.jsx
@@ -2,9 +2,9 @@
 
 export default function LinkToEditor() {
   return (
-    <div>
+    <>
 
  <a href="https://liveblocks-red.vercel.app/" target="_blank" classname="text-indigo-300 decoration-none ">Colloborate with Editor</a>
-    </div>
+    </>
   );
 }

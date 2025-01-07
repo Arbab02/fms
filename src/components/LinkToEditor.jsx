31671dbe05ba2e4a8f3fea8d5eaeb7e875f3ffa6
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2025-01-07 08:52:34 -0800

    Update LinkToEditor.jsx

--- src/components/LinkToEditor.jsx
+++ src/components/LinkToEditor.jsx
@@ -6,9 +6,8 @@ export default function Page() {
   const router = useRouter()
  
   return (
-    <button className="bg-gray-900 hover:bg-gray-800 text-white py-2 px-4 rounded-lg shadow" type="button" onClick={() => router.push('https://liveblocks-red.vercel.app/')}>
-      Collaborate 
-     <FaRocketchat className="w-5 h-5"/>
+    <button className="bg-gray-900 hover:bg-gray-800 text-white py-3 px-8 rounded-lg shadow" type="button" onClick={() => router.push('https://liveblocks-red.vercel.app/')}>
+     <FaRocketchat className="w-5 h-5 inline-block"/> Collaborate
      
     </button>
   )

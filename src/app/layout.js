793e3b0414b9381b285850f5f5e7fa11e5d41d05
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2025-01-06 21:03:52 -0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from '@/components/Navbar.jsx'
-// import LinkToEditor from '@/components/LinkToEditor.jsx'
+import LinkToEditor from '@/components/LinkToEditor.jsx'
 
 import Footer from '@/components/Footer.jsx'
 // import { ClerkProvider } from '@clerk/nextjs';
@@ -20,6 +20,7 @@ export default function RootLayout({ children }) {
      
       <body className={inter.className}>
       <Navbar/>
+      <LinkToEditor/>
       
      
         {children}

88f37d73a9f6ccc81f13ba9e9d683339d730ee4d
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2025-01-07 08:39:51 -0800

    Update LinkToEditor.jsx

--- src/components/LinkToEditor.jsx
+++ src/components/LinkToEditor.jsx
@@ -1,5 +1,5 @@
 'use client'
- 
+ import { FaRocketchat } from "react-icons/fa";
 import { useRouter } from 'next/navigation'
  
 export default function Page() {
@@ -7,7 +7,9 @@ export default function Page() {
  
   return (
     <button className="bg-gray-900 hover:bg-gray-800 text-white py-2 px-4 rounded-lg shadow" type="button" onClick={() => router.push('https://liveblocks-red.vercel.app/')}>
-      Collaborate
+      Collaborate 
+     <FaRocketchat className="w-5 h-5"/>
+     
     </button>
   )
 }
